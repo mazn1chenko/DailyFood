b2b4d5d708696cdddd7afca89225877ae7ad4083
mazn1chenko <maznichenko.144@gmail.com>
2023-03-16 14:29:28 +0200

    Start of development "DailyFood"
    Start of development "DailyFood"

---
+++ DailyFood/DailyFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyFood
+//
+//  Created by m223 on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
